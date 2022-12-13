48a1f2c1e60e31f5ac5ec6e859f99c18c3b21059
Daniel9822 <dhdany64@gmail.com>
2022-12-12 22:49:15 -0400

    redux-toolkit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../reducers/prueba/pruebaSlider";
+
 function Home (){
+    const dispatch = useDispatch()
+    const { users } = useSelector(state => state.prueba)
+
+    useEffect(() => {
+        dispatch(addUser(['Daniel', 'Pablo', 'Macarena', 'Aylen', 'K-Itzel', 'Mauro', 'LOS DEMAS NO ME SE SUS NOMBRES']))
+    },[dispatch])
 return(
     <div>
         <h1>Proyecto final Henry ---tamplate</h1>
+        <h2>integrantes: {users && users.map(ele => ele).join(', ')}</h2>
         <h1>Primer commit a develop</h1>
     </div>
 )
