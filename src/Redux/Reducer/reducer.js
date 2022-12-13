ca3574a0bdbfa6e338dd24a36f0ea37595266e6a
AylenGorosito313 <gorovicio31@gmail.com>
2022-12-12 21:16:58 -0300

    first commit

---
+++ src/Redux/Reducer/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  element: [],
+};
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+};
+
+export default rootReducer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+function Home (){
+return(
+    <div>
+        <h1>Proyecto final Henry ---tamplate</h1>
+    </div>
+)
+}
+export default Home
