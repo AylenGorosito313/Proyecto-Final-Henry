2c9503e15ad97dc9bfdbf787a4ca462a99647273
Aylen Gorosito <109639386+AylenGorosito313@users.noreply.github.com>
2022-12-13 23:15:49 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,20 +2,33 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../reducers/prueba/pruebaSlider";
 
-function Home (){
-    const dispatch = useDispatch()
-    const { users } = useSelector(state => state.prueba)
+function Home() {
+  const dispatch = useDispatch();
+  const { users } = useSelector((state) => state.prueba);
 
-    useEffect(() => {
-        dispatch(addUser(['Daniel', 'Pablo', 'Macarena', 'Aylen', 'K-Itzel', 'Mauro', 'LOS DEMAS NO ME SE SUS NOMBRES']))
-    },[dispatch])
-return(
+  useEffect(() => {
+    dispatch(
+      addUser([
+        "Daniel",
+        "Pablo",
+        "Macarena",
+        "Aylen",
+        "K-Itzel",
+        "Mauro",
+        "LOS DEMAS NO ME SE SUS NOMBRES",
+        "algo",
+      ])
+    );
+  }, [dispatch]);
+  return (
     <div>
-        <h1>Henry Games Ultimate</h1>
-        <h1>Proyecto final Henry ---tamplate</h1>
-        <h2>integrantes: {users && users.map(ele => ele).join(', ')}</h2>
-        <h1>Primer commit a develop</h1>
+      <h1>Amigos la buena soy pablo de bayordrigans</h1>
+      <h1>Games Ultimate</h1>
+
+      <h1>Proyecto final Henry ---tamplate</h1>
+      <h2>integrantes: {users && users.map((ele) => ele).join(", ")}</h2>
+      <h1>Primer commit a develop</h1>
     </div>
-)
+  );
 }
-export default Home
+export default Home;
